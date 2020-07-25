9d6fd28a01d2ed28408c2c557d8d67e5085dd4c9
Víctor Colombo <victorcora98@gmail.com>
2020-07-25 15:15:00 -0300

    Implement task deletion

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -60,3 +60,19 @@ exports.update = async (req, res, next) => {
         res.status(400).json(error);
     }
 }
+
+exports.destroy = async (req, res, next) => {
+    const id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(id);
+
+        if(!task) {
+            return res.status(404).json();
+        }
+
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
--- src/routes/tasksRoute.js
+++ src/routes/tasksRoute.js
@@ -7,6 +7,7 @@ router.post('/', controller.create);
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.patch('/:id', controller.update);
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
 
